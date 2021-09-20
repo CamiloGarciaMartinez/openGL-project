718f694f2de739ba09e4557ce3c90b62efe2a1bd
CamiloGarciaMartinez <camilogarciamartinez@protonmail.com>
2021-09-19 22:24:52 -0300

    add VBO class

--- include/VBO.cpp
+++ include/VBO.cpp
@@ -1,1 +1,20 @@
 #include "VBO.h";
+
+VBO::VBO(GLfloat* vertices, GLsizeiptr size) {
+  glGenBufers(1, &ID);  // generate Vertex Buffer Object
+  
+  glBindBuffer(GL_ARRAY_BUFFER, ID);  // bind to the current context
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), GL_STATIC_DRAW);  // load data to the buffer
+}
+
+void VBO::Bind() {
+  glBindBuffer(GL_ARRAY_BUFFER, ID);
+}
+
+void VBO::Unbind() {
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VBO::Delete() {
+  glDeleteBuffers(1, &ID);
+}
