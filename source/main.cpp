480bdd6b0c8606e84b56bffe6e61294f993c9027
CamiloGarciaMartinez <camilogarciamartinez@protonmail.com>
2021-09-28 16:02:51 -0300

    fix shader error

--- source/main.cpp
+++ source/main.cpp
@@ -1,102 +1,126 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include<iostream>
+#include<cmath>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 
-#include <iostream>
-#include <cmath>
-#include <fstream>
-#include <string>
-#include "shader.h"
-
-#include "VBO.h"
-#include "VAO.h"
-#include "EBO.h"
-
-int main(int argc, char* argv[]) {
-  // Init GLFW and register openGL version/profile
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // Vertices of the figuere we want to render
-  GLfloat vertices[] = {
-    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,      0.8f, 0.3f, 0.02f,  // bottom left vertice
-     0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,      0.8f, 0.3f, 0.02f,  // bottom right vertice
-     0.5f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f, 0.6f, 0.32f,  // upper right vertice
-    -0.5f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   0.9f, 0.45f, 0.17f, // upper left vertice
-  };
-  GLuint indices[] = {
-    0, 3, 5,  // lower left triangle
-    3, 2, 4,  // lower right triangle
-    5, 4, 1   // upper triangle
-  };
-  GLuint squareIndices[] = {
-    0, 1, 2,
-    0, 2, 3
-  };
-  GLfloat textureCoords[] = {
-    0.0f, 0.0f, // lower-left corner
-    1.0f, 0.0f, // lower-right corner
-    0.0f, 1.0f, // top-left corner
-    1.0f, 1.0   // top-right corner
-  };
-
-  // Create window, if failed terminate program
-  GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
-  if ( window == NULL )  {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-  }
-  // Set current conxtext to window
-  glfwMakeContextCurrent(window);
-  // Init glad
-  gladLoadGL();
-  // Set viewport
-  glViewport(0, 0, 800, 800);
+#include"shader.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
 
 
-  std::string vertexShaderPath = "../resources/shaders/default.vert";
-  std::string fragmentShaderPath = "../resources/shaders/default.frag";
-  Shader shader(vertexShaderPath.c_str(), fragmentShaderPath.c_str());
-  // Create and initilize Vertex Arrays Object ande Vertex Buffer Object (the order is important)
-  VAO VAO01;
-  VAO01.Bind();
 
-  VBO VBO01(vertices, sizeof(vertices));
-  EBO EBO01(squareIndices, sizeof(squareIndices));
+// Vertices coordinates
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+};
 
-  VAO01.LinkAttrib(VBO01, 0, 4, GL_FLOAT, 6 * sizeof(float), (void*)0);
-  VAO01.LinkAttrib(VBO01, 1, 4, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-
-  VBO01.Unbind();
-  VAO01.Unbind(); 
-  EBO01.Unbind();
-
-
-  // Change background color
-  glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glfwSwapBuffers(window);
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
 
 
-  // This while loop prevents the program to terminate before requested
-  while(!glfwWindowShouldClose(window)) {
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    shader.Activate();
-    VAO01.Bind();
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
 
 
-    glfwSwapBuffers(window);  // swap screen buffers  to  refresh the window
-    // Handle incomming events
-    glfwPollEvents();
-  }
-  VAO01.Delete();
-  VBO01.Delete();
-  EBO01.Delete();
-  shader.Delete();
-  // Terminate program
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+
+	// Generates Shader object using shaders defualt.vert and default.frag
+	Shader shaderProgram("resources/shaders/default.vert", "resources/shaders/default.frag");
+
+
+
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	// Gets ID of uniform called "scale"
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		shaderProgram.Activate();
+		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+		glUniform1f(uniID, 0.5f);
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
 }
